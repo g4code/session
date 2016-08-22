95ad93113fee3ddfdac8d1936a940d4e9920cac1
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-08-22 11:46:23 +0200

    Added session regenerate id at destroy

--- src/Session.php
+++ src/Session.php
@@ -47,6 +47,7 @@ class Session
         ]);
         $params = session_get_cookie_params();
         setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
+        session_regenerate_id(true);
     }
 
     /**
